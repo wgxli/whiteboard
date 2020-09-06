163417a17b3abb0e39a4fdb2ec52e1e85caedf84
Samuel Li <git@samuelj.li>
2020-09-05 21:37:35 -0400

    Updates

---
+++ precache-manifest.00f517ac6d90a38b404e4e53048a33bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80661842ac369e365a9dc812925c159c",
+    "url": "/whiteboard/index.html"
+  },
+  {
+    "revision": "8195711292117c64c530",
+    "url": "/whiteboard/static/css/main.68070bac.chunk.css"
+  },
+  {
+    "revision": "acda48cdfa6c97789332",
+    "url": "/whiteboard/static/js/2.e11ced29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteboard/static/js/2.e11ced29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8195711292117c64c530",
+    "url": "/whiteboard/static/js/main.04c9d31f.chunk.js"
+  },
+  {
+    "revision": "95f52f808f7b1ca668e1",
+    "url": "/whiteboard/static/js/runtime-main.c2696591.js"
+  }
+]);
